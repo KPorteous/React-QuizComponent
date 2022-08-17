87020d03375b70996829ce616842f701fa749d08
KPorteous <kieran.porteous@groubook.co.uk>
2022-08-17 11:36:32 +0100

    mod 2 wip

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,13 +5,17 @@ let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
     constructor(props) {
-        super(props);
-        this.state = {quiz_position: 1};
-    };
+        super(props)
+        this.state = { quiz_position: 1 }
+    }
 
     render() {
-        return <QuizQuestion quiz_quistion={quizData.quiz_questions[this.state.quiz_position -1]}/>;
-    };
-};
+        return (
+            <div>
+                <QuizQuestion quiz_quistion={quizData.quiz_questions[this.state.quiz_position -1]} />
+            </div>
+        )
+    }
+}
 
-export default Quiz;
+export default Quiz
